2236b2b761c9fcd7059e7fa4e21c604ef10f21b8
minjikim <inkmjj@sookmyung.ac.kr>
2023-11-14 15:17:01 +0900

    [Feat(라이/김민지)] 연관관계 추가

--- src/main/java/com/umc/demo/domain/ReviewImage.java
+++ src/main/java/com/umc/demo/domain/ReviewImage.java
@@ -20,22 +20,15 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Review extends BaseEntity {
+public class ReviewImage extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50)
-    private String title;
-
-    private Float score;
+    private String imageUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "store_id")
-    private Store store;
+    @JoinColumn(name = "review_id")
+    private Review review;
 }
--- src/main/java/com/umc/demo/domain/Store.java
+++ src/main/java/com/umc/demo/domain/Store.java
@@ -3,9 +3,12 @@ package com.umc.demo.domain;
 import com.umc.demo.domain.common.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,4 +33,8 @@ public class Store extends BaseEntity {
     private String address;
 
     private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
 }
