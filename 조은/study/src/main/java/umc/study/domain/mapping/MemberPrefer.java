f6cc6a528de5795fff4ce165f550ffd36c1a30cc
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-11-21 11:16:32 +0900

    [Feat]: 매핑 엔티티들 생성

---
+++ 조은/study/src/main/java/umc/study/domain/mapping/MemberPrefer.java
@@ -0,0 +1,19 @@
+package umc.study.domain.mapping;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberPrefer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
