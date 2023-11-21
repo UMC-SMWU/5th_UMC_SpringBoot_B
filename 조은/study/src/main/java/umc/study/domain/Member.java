663f88cd2a81937735bb7c1b07169bddef9b84c4
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-11-21 10:56:05 +0900

    [Feat]: Member 엔티티 생성

---
+++ 조은/study/src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,18 @@
+package umc.study.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
