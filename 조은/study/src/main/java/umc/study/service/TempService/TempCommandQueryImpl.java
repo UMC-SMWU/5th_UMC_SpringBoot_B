2ba6bd7811ad556498ece9e13c88b0c005cf01ed
givesilverstick <silverstick21@sookmyung.ac.kr>
2023-11-27 00:17:45 +0900

    [Feat]: 8주차 - Exception 핸들링 하기 (에러 수정)

--- 조은/study/src/main/java/umc/study/service/TempService/TempCommandQueryImpl.java
+++ 조은/study/src/main/java/umc/study/service/TempService/TempCommandQueryImpl.java
@@ -1,9 +1,11 @@
 package umc.study.service.TempService;
 
 import lombok.*;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class TempCommandQueryImpl implements TempQueryService{
 
